430966be172afaf0f6a05738d58738ef34020b3f
Peter Griess <pg@std.in>
2010-08-16 10:27:51 -0500

    Initial implementation.
    - Includes parse() function which streams big and little-endian uint8
      values, unit test for same.

---
+++ test/stream.js
@@ -0,0 +1,37 @@
+// A mock stream implementation that breaks up provided data into
+// random-sized chunks and emits 'data' events. This is used to simulate
+// data arriving with arbitrary packet boundaries.
+
+var Buffer = require('buffer').Buffer;
+var EventEmitter = require('events').EventEmitter;
+var sys = require('sys');
+
+var Stream = function(str, min, max) {
+    EventEmitter.call(this);
+
+    min = min || 1;
+    max = max || str.length;
+    
+    var self = this;
+    var buf = new Buffer(str, 'utf-8');
+
+    var emitData = function() {
+        var len = Math.min(
+            min + Math.floor(Math.random() * (max - min)),
+            buf.length
+        );
+
+        var b = buf.slice(0, len);
+
+        if (len < buf.length) {
+            buf = buf.slice(len, buf.length);
+            process.nextTick(emitData);
+        }
+
+        self.emit('data', b);
+    };
+
+    process.nextTick(emitData);
+};
+sys.inherits(Stream, EventEmitter);
+exports.Stream = Stream;
---
+++ test/test-uint8.js
@@ -0,0 +1,32 @@
+// Test reading uint8 values in different endiannesses. Regardless, the
+// value should be the same.
+
+var assert = require('assert');
+var Stream = require('./stream').Stream;
+var strtok = require('../lib/strtok');
+
+var seen = 0;
+
+strtok.parse(new Stream('\x1a\x1a\x1a\x1a\x1a\x1a'), function(v) {
+    if (v === undefined) {
+        return strtok.Types.UINT8_LE;
+    }
+
+    switch (seen++ % 1) {
+    case 0:
+        assert.ok(v === 0x1a);
+        return (seen < 6) ?
+            strtok.Types.UINT8_BE :
+            strtok.Types.DONE;
+
+    case 1:
+        assert.ok(v === 0x1a);
+        return (seen < 6) ?
+            strtok.Types.UINT8_LE :
+            strtok.Types.DONE;
+    }
+});
+
+process.on('exit', function() {
+    assert.equal(6, seen);
+});
