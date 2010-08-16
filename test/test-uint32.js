03bb3b3f46bd4830d58c847205b34d7580aa9188
Peter Griess <pg@std.in>
2010-08-16 16:17:04 -0500

    Add support for 32-bit unsigned integers.

--- test/test-uint32.js
+++ test/test-uint32.js
@@ -1,4 +1,4 @@
-// Test reading uint16 values in different endiannesses.
+// Test reading uint32 values in different endiannesses.
 
 var assert = require('assert');
 var TestStream = require('./util').TestStream;
@@ -6,22 +6,22 @@ var strtok = require('../lib/strtok');
 
 var seen = 0;
 
-strtok.parse(new TestStream('\x1a\x00\x1a\x00\x1a\x00\x1a\x00'), function(v) {
+strtok.parse(new TestStream('\x1a\x00\x1a\x00\x1a\x00\x1a\x00\x1a\x00\x1a\x00\x1a\x00\x1a\x00'), function(v) {
     if (v === undefined) {
-        return strtok.Types.UINT16_LE;
+        return strtok.Types.UINT32_LE;
     }
 
     switch (seen++ % 2) {
     case 0:
-        assert.equal(v, 0x001a);
+        assert.equal(v, 0x001a001a);
         return (seen < 4) ?
-            strtok.Types.UINT16_BE :
+            strtok.Types.UINT32_BE :
             strtok.Types.DONE;
 
     case 1:
-        assert.equal(v, 0x1a00);
+        assert.equal(v, 0x1a001a00);
         return (seen < 4) ?
-            strtok.Types.UINT16_LE :
+            strtok.Types.UINT32_LE :
             strtok.Types.DONE;
     }
 });
