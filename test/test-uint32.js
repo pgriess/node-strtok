04b869c3f6f05735ab9880b76e93b8cbfcca1549
Peter Griess <pg@std.in>
2010-08-21 17:51:07 -0500

    Support uint32 generators.

--- test/test-uint32.js
+++ test/test-uint32.js
@@ -1,8 +1,29 @@
-// Test reading uint32 values in different endiannesses.
+// Test writing and reading uint32 values in different endiannesses.
 
 var assert = require('assert');
 var util = require('./util');
 var strtok = require('../lib/strtok');
+
+util.runGenerateTests(
+    [function(s) {
+        strtok.UINT32_LE.put(s, 0);
+    }, '\x00\x00\x00\x00'],
+    [function(s) {
+        strtok.UINT32_LE.put(s, 0xff);
+    }, '\xff\x00\x00\x00'],
+    [function(s) {
+        strtok.UINT32_BE.put(s, 0);
+    }, '\x00\x00\x00\x00'],
+    [function(s) {
+        strtok.UINT32_BE.put(s, 0xff);
+    }, '\x00\x00\x00\xff'],
+    [function(s) {
+        strtok.UINT32_LE.put(s, 0xaabbccdd);
+    }, '\xdd\xcc\xbb\xaa'],
+    [function(s) {
+        strtok.UINT32_BE.put(s, 0xaabbccdd);
+    }, '\xaa\xbb\xcc\xdd']
+);
 
 var le = function(v) {
     assert.equal(v, 0x001a001a);
